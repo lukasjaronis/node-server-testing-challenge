cd61b190f4297407ba9079b9a32e2b6c6c610a72
lukasjaronis <jaronisluke13@gmail.com>
2020-01-30 19:05:43 -0500

    everything is done. 1 test passed, the other is having problems.

---
+++ companies/spec.js
@@ -0,0 +1,24 @@
+const db = require('../data/db-Config');
+const CompanyModel = require('./model');
+
+describe('company model', () => {
+    describe('insert()', () => {
+        it('should insert a company', async () => {
+            await CompanyModel.insert({ companyName: 'Amazon', founder: 'Jeff Bezos' });
+
+            const companies = await db('companies');
+            expect(companies.length).not.toBeLessThan(1)
+        })
+    });
+});
+
+describe('remove()', () => {
+    it('should delete a company', async () => {
+        await CompanyModel.remove({ id: 1 });
+        const companies = await db('companies');
+        expect(companies).not.toContain({
+            companyName: 'ICBC',
+            founder: 'Jiang Jianqing'
+        })
+    });
+});
